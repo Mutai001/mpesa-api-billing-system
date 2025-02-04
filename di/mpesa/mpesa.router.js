508b03b409d39d83c274990bbe2e8b0468dcaa55
mutai001 <omusekim@gmail.com>
2025-02-04 09:00:23 +0300

    dist conflict

--- di/mpesa/mpesa.router.js
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { initiateStkPush, stkCallback } from "./mpesa.controller.js";
-const mpesaRouter = new Hono();
-mpesaRouter.post("/stkpush", initiateStkPush);
-mpesaRouter.post("/callback", stkCallback);
-export default mpesaRouter;
